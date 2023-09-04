e84eac2f19aaa9d264d7aee1b1892a40621d757c
G-macbook <guheshpanjagall@G-MacBook-Air.local>
2023-09-04 15:13:16 +0530

    add main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+
+}
+
+resource "aws_instance" "namaste" {
+  ami           = "ami-06f621d90fa29f6d0"
+  instance_type = "t2.micro"
+}
+
+
+
+output "instance_id" {
+  value = aws_instance.namaste.id
+
+}
