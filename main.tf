c2c5d20b8947da5bd40453d73b2d3187b1dd9847
G-macbook <guheshpanjagall@G-MacBook-Air.local>
2023-09-04 16:43:26 +0530

    creating s3 backend and vpc in main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,62 @@ resource "aws_instance" "namaste" {
 }
 
 
+resource "aws_s3_bucket" "gps3" {
+  bucket = "gp-tf-gps3-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.gps3.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.gps3.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.gps3.id
+  acl    = "public-read"
+}
+
 
 output "instance_id" {
   value = aws_instance.namaste.id
 
+
+}
+output "aws_s3_bucket" {
+  value = aws_s3_bucket.gps3.id
+
+
+}
+terraform {
+  backend "s3" {
+    bucket = "gp-tf-gps3-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+resource "aws_vpc" "gp_vpc" {
+  cidr_block = "10.0.0.0/20"
+
+
+}
+
+output "vpc_id" {
+  value = aws_vpc.gp_vpc.id
 
 }
